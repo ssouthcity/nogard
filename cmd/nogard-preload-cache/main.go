2191198a372e4293d3cfcb6c7829c6b5dedff376
Stian Sørby <ssouthcity@gmail.com>
2022-06-27 18:52:05 +0200

    cache script

---
+++ cmd/nogard-preload-cache/main.go
@@ -0,0 +1,43 @@
+package main
+
+import (
+	"flag"
+	"os"
+
+	"github.com/sirupsen/logrus"
+	"github.com/ssouthcity/nogard"
+	"github.com/ssouthcity/nogard/compendium"
+	"github.com/ssouthcity/nogard/fandom"
+	"github.com/ssouthcity/nogard/redis"
+)
+
+func main() {
+	logger := logrus.New()
+
+	sheetID := flag.String("sheetID", os.Getenv("NOGARD_COMPENDIUM_SHEET_ID"), "")
+	sheetCreds := flag.String("credentialsPath", os.Getenv("NOGARD_COMPENDIUM_SHEET_CREDENTIALS"), "")
+	flag.Parse()
+
+	var dragonSrv nogard.DragonEncyclopedia
+	{
+		dragonSrv, _ = compendium.NewDragonEncyclopedia(*sheetID, *sheetCreds)
+		dragonSrv = fandom.NewDragonDescriptionPatcher(dragonSrv)
+		dragonSrv = redis.NewDragonCache(dragonSrv, logger)
+	}
+
+	names, err := dragonSrv.DragonNames()
+	if err != nil {
+		logger.WithError(err).Fatal("unable to get dragon names")
+	}
+
+	for _, name := range names {
+		d, err := dragonSrv.Dragon(name)
+		if err != nil {
+			logger.WithError(err).WithField("name", name).Warn("unable to cache dragon")
+		}
+
+		logger.WithField("dragon", d.Name).Info("cached dragon")
+	}
+
+	logger.Info("success")
+}
